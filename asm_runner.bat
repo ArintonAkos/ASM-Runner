79de60d4be41a4079ddb0911b065bacced60a6f7
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2021-10-18 23:26:27 +0300

    First commit

---
+++ asm_runner.bat
@@ -0,0 +1,10 @@
+@echo off
+start cmd /k "nasm -f win32 %1.asm"
+timeout /t 1
+start cmd /k "nlink %1.obj -lio -o %1.exe"
+timeout /t 1
+start cmd /k "%1.exe"
+timeout /t 1
+if NOT [%2]==[] (
+	start cmd /k "actest.exe %2 %1.exe"
+)
