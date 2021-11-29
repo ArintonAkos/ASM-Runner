5eca8eee91d6526c28bd0eafea829929a9890a67
ArintonAkos <43067524+ArintonAkos@users.noreply.github.com>
2021-11-29 13:46:33 +0200

    Update asm_runner.bat

--- asm_runner.bat
+++ asm_runner.bat
@@ -1,10 +1,6 @@
 @echo off
-start cmd /k "nasm -f win32 %1.asm"
-timeout /t 1
-start cmd /k "nlink %1.obj -lio -lmio -o %1.exe"
-timeout /t 1
-start cmd /k "%1.exe"
-timeout /t 1
+start cmd /k "nasm -f win32 ionum.asm & nasm -f win32 iostr.asm & nasm -f win32 strings.asm & nasm -f win32 %1.asm & timeout 2 /nobreak & nlink %1.obj ionum.obj iostr.obj strings.obj -lmio -lio -o %1.exe & timeout 1 /nobreak & echo Here starts your program & %1.exe"
+timeout	/t 4
 if NOT [%2]==[] (
 	start cmd /k "actest.exe %2 %1.exe"
 )
